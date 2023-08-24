1c6a5e11e24e103cbd21f2b7455b6d2eb06d5594
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-08-24 18:30:23 +0200

    create auth and students

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
-
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { StudentsModule } from './students/students.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, StudentsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
